f3a33ffd36f754981c5232e98fa1d16f97542727
eduardokreve <eduardokreve283@gmail.com>
2016-03-09 15:09:13 -0300

    Create Exe01_Alocação.c

---
+++ Exe01_Alocação.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int ver,i,tamanho=5;
+  
+	int *vet = (int*) malloc(tamanho *sizeof(int));
+
+    for(i=0;i<tamanho;i++){
+	
+		ver = scanf("%d",&vet[i]);
+		if(ver==0){
+			free(vet);
+			return 0;
+		}
+		
+		else{
+			if(i%5>0){
+				tamanho+=5;
+				vet = (int*) realloc(vet, tamanho *sizeof(int));
+			}	
+		}
+		i++;
+	}
+  
+  	return 0;
+}
