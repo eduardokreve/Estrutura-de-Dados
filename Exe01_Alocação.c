01da94b4998df3373d6bd0068f4d4b0a1a755d39
eduardokreve <eduardokreve283@gmail.com>
2016-03-09 15:11:15 -0300

    Update Exe01_Alocação.c

--- Exe01_Alocação.c
+++ Exe01_Alocação.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 int main(){
-	int ver,i,tamanho=5;
+    int ver,i,tamanho=5;
   
-	int *vet = (int*) malloc(tamanho *sizeof(int));
+    int *vet = (int*) malloc(tamanho *sizeof(int));
 
     for(i=0;i<tamanho;i++){
 	
-		ver = scanf("%d",&vet[i]);
-		if(ver==0){
-			free(vet);
-			return 0;
-		}
+        ver = scanf("%d",&vet[i]);
+	if(ver==0){
+	    free(vet);
+	    return 0;
+	}
 		
-		else{
-			if(i%5>0){
-				tamanho+=5;
-				vet = (int*) realloc(vet, tamanho *sizeof(int));
-			}	
-		}
-		i++;
+	else{
+	    if(i%5>0){
+	        tamanho+=5;
+		vet = (int*) realloc(vet, tamanho *sizeof(int));
+	    }	
+	}
+	i++;
 	}
   
   	return 0;
