26ace75d354cc1d20fdf1bddb17364997968cc3a
eduardokreve <eduardokreve283@gmail.com>
2016-08-19 08:43:51 -0300

    Update Exercicio_aula.c

--- Exercicio_aula.c
+++ Exercicio_aula.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 int main(){
-  int i=0,j=0,linhas;
+  int i = 0, j = 0, linhas;
 	
   printf("Digite a quant. de linhas: ");
-  scanf("%d",&linhas);
+  scanf("%d", &linhas);
 
   int colunas[linhas];
-  int **matriz =malloc(linhas * sizeof (int));
+  int **matriz = malloc(linhas * sizeof (int));
 	
-  for(i=0;i<linhas;i++){
+  for(i = 0; i < linhas; i++){
     printf("\nNumero de colunas: ");
-    scanf("%d",&colunas[i]);
+    scanf("%d", &colunas[i]);
 		
     matriz[i] = malloc(colunas[i] * sizeof (int));
-    for(j=0;j<colunas[i];j++){
+    for(j = 0; j < colunas[i]; j++){
       matriz[i][j] = rand()%100;
-     //	printf("%p ",&matriz[i]); //imprime o endereço de cada valor
-      printf("%d  ",matriz[i][j]);
+     //	printf("%p ", &matriz[i]); //imprime o endereço de cada valor
+      printf("%d  ", matriz[i][j]);
     }
   } 
   printf("\n");
